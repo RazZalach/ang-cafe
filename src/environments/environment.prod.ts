55410c1455588958b8aea1f14a511fa9c6496e55
Raz <110323932+Idus-Raz@users.noreply.github.com>
2023-06-25 11:49:38 +0300

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+// lien de base de donnees
+// lien de base de donnees
+export const environment = {
+  production: true,
+   firebaseConfig :{
+  apiKey: "AIzaSyC1fv1BOfdb81TO4N5KYJjNSboWKz6S3nM",
+  authDomain: "caffepost.firebaseapp.com",
+  projectId: "caffepost",
+  storageBucket: "caffepost.appspot.com",
+  messagingSenderId: "1007980331968",
+  appId: "1:1007980331968:web:c33c1b38652c14af31be25"
+}
+};
